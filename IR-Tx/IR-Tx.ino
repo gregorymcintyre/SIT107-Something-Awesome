980e127e7b77efd9bf8e56a8ed18b3a4a589fa43
gregorymcintyre <gregmcintyre1984@gmail.com>
2018-08-01 12:42:46 +1000

    NEC Tx attempts

--- IR-Tx/IR-Tx.ino
+++ IR-Tx/IR-Tx.ino
@@ -7,10 +7,13 @@ void setup()
 }
 
 void loop() {
-  if (Serial.read() != -1) {
-    for (int i = 0; i < 3; i++) {
-      irsend.sendNEC(EAD02569, 32);  //HEX of Rx Data
+  //if (Serial.read() != -1) {
+    //for (int i = 0; i < 3; i++) {
+      irsend.sendNEC(0xEAD02569, 32);  //HEX of Rx Data
+      //irsend.sendNEC(0x7200000, 32);
+      irsend.sendNEC(0xFFFFFFFF, 32);  //HEX of Rx Data
+      Serial.println("Tx");
       delay(5000);
-    }
-  }
+    //}
+  //}
 } 
