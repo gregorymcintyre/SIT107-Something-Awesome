0d05928f65910bb0aa7bda3ed1af51e6b8cf222b
Greg McIntyre <gregmcintyre1984@gmail.com>
2018-08-01 12:15:17 +1000

    NEC test version

--- IR-Tx/IR-Tx.ino
+++ IR-Tx/IR-Tx.ino
@@ -9,8 +9,8 @@ void setup()
 void loop() {
   if (Serial.read() != -1) {
     for (int i = 0; i < 3; i++) {
-      irsend.sendSony(0xa90, 12); // Sony TV power code
-      delay(100);
+      irsend.sendNEC(EAD02569, 32);  //HEX of Rx Data
+      delay(5000);
     }
   }
 } 
