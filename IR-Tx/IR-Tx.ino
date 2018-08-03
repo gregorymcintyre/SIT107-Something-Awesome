4d9afb29ffae2d8d8057c149984ca9bb1f663b87
Greg McIntyre <gregmcintyre1984@gmail.com>
2018-08-03 11:58:01 +1000

    commenting updates

--- IR-Tx/IR-Tx.ino
+++ IR-Tx/IR-Tx.ino
@@ -15,15 +15,16 @@
 #define DHTPIN 2     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 #define KHZ 38
-#define OPTIMALTEMP 30
-#define OPTIMALHUM 90
+#define OPTIMALTEMP 30  //set optimal temp here
+#define OPTIMALHUM 90   //set optimal humm here
 
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
 
 
 //Variables
 IRsend irsend;
-bool flag;
+bool flag;  //indicates if message needs to be sent
+bool sent;  //indicates if message was sent
 float hum;  //Stores humidity value
 float temp; //Stores temperature value
 
@@ -37,9 +38,6 @@ void loop() {
 
   hum = dht.readHumidity();
   temp= dht.readTemperature();
-  flag = 0;
-
-  if(hum>=OPTIMALHUM && temp>=OPTIMALTEMP){
 
   /*
   Serial.print("Humidity: ");   //error checking
@@ -48,17 +46,26 @@ void loop() {
   Serial.print(temp);
   Serial.println(" Celsius");
   */
+  
+  flag = 0;
+  sent = 0;
 
-  flag =1;
-   
-  //Serial.println("Tx"); //to indicate Trasmission during testing
+  if(hum>=OPTIMALHUM && temp>=OPTIMALTEMP){
+
+    flag = 1;                     //indicates code needs to be sent
+
+  }else{
+
+    sent = 0;                   //no message has been sent (also gives portenital for the program to turn off the sircon when it is under temp in future iterations
+  
   }
 
-  if (flag) {
+  if (flag && !sent) {
     unsigned int irSignal[] = {3500, 1650, 450, 400, 450, 1250, 450, 350, 450, 400, 450, 350, 500, 350, 450, 400, 450, 350, 450, 400, 450, 350, 450, 400, 450, 350, 450, 400, 450, 1250, 450, 350, 500, 350, 450, 400, 450, 350, 450, 400, 450, 350, 450, 400, 450, 1250, 450, 1250, 450, 1250, 450, 400, 450, 350, 450, 1300, 450, 350, 450, 350, 500, 350, 450, 350, 500, 350, 450, 400, 450, 350, 450, 400, 450, 350, 450, 400, 450, 350, 450, 400, 450, 400, 450, 350, 450, 400, 450, 350, 450, 400, 450, 350, 450, 400, 450, 350, 450, 400, 450}; //AnalysIR Batch Export (IRremote) - RAW
-  irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), KHZ); //Note the approach used to automatically calculate the size of the array.
+    irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), KHZ); //Note the approach used to automatically calculate the size of the array.
+    //Serial.println("Tx"); //to indicate Trasmission during testing
+    sent = 1;                                   //indicates code has been sent to reduce excessive sending.
   }
   
-  delay(2000);
-  
+  delay(2000);      //sample every 2 seconds
 }
